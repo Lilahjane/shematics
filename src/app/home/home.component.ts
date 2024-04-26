a6f34fe9be60938c2164bed4e63ed5e0666abe29
Lilah Jane <lilahjane25@gmail.com>
2024-04-26 09:46:52 -0400

    IT FINALLY WORKS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { RecipeService } from './../recipe.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
@@ -8,7 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Recipe,} from '../recipe';
 import { HttpClientModule } from '@angular/common/http';
-
+import { Router } from '@angular/router';
+import { MatPaginatorModule } from '@angular/material/paginator';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -22,16 +23,118 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatCardModule,
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatPaginatorModule
   ],
   providers:[RecipeService]
 })
+
+
 export class HomeComponent implements OnInit {
-  recipes: Recipe[] = [];
+  private service = inject(RecipeService);
+  private router = inject(Router);
+  private recipesPerPage: number = 12;
+  private totalPages!: number;
+  public recipes: Recipe[] = [];
+  public visibleRecipes: Recipe[] = [];
+  public activePage: number = 1;
 
-  constructor(private recipeService: RecipeService) {}
+  ngOnInit(): void {
+    this.gerRecipes();
+  }
 
-  ngOnInit() {
-    this.recipeService.getAllRecipes().subscribe((data) => (this.recipes = data));
+  private gerRecipes(): void {
+    this.service.getAllRecipes().subscribe((recipes) => {
+      this.initPagination(recipes);
+      this.setRecipes(recipes);
+      this.setPagination();
+    });
+  }
+
+  private initPagination(recipes: Recipe[]) {
+    this.totalPages = Math.ceil(recipes.length / this.recipesPerPage);
+  }
+
+  private setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes.map((item, index) => {
+      return { ...item, id: index + 1 };
+    });
+  }
+
+  private setPagination() {
+    const startIndex = (this.activePage - 1) * this.recipesPerPage;
+    const endIndex = startIndex + this.recipesPerPage;
+    this.visibleRecipes = this.recipes.slice(startIndex, endIndex);
+  }
+
+  public handlePageChange(event: any) { // Handle page change event from paginator
+    this.activePage = event.pageIndex + 1; // Adjust for 1-based indexing
+    this.setPagination();
+  }
+
+  public viewRecipe(recipe: Recipe) {
+    this.router.navigateByUrl(`recipe-details/${recipe.id}`);
   }
 }
+
+// export class HomeComponent implements OnInit {
+//   private service = inject(RecipeService);
+//   private router = inject(Router);
+//   private recipesPerPage: number = 12;
+//   private totalPages!: number;
+//   private recipes: Recipe[] = [];
+//   public pages: number[] = [];
+//   public activePage: number = 1;
+//   public visibleRecipes: Recipe[] = [];
+
+//   ngOnInit(): void {
+//     this.gerRecipes();
+//   }
+
+//   private gerRecipes(): void {
+//     this.service.getAllRecipes().subscribe((recipes) => {
+//       this.initPagination(recipes);
+//       this.setRecipes(recipes);
+//       this.setPagination();
+//     });
+//   }
+
+//   private initPagination(recipes: Recipe[]) {
+//     this.totalPages = Math.ceil(recipes.length / this.recipesPerPage);
+//     if (this.totalPages > 0) {
+//       this.pages = [...Array(this.totalPages).keys()].map((i) => i + 1);
+//     }
+//   }
+
+//   private setRecipes(recipes: Recipe[]) {
+//     this.recipes = recipes.map((item, index) => {
+//       return { ...item, id: index + 1 };
+//     });
+//   }
+
+//   private setPagination() {
+//     const paginationStart = (this.activePage - 1) * this.recipesPerPage;
+//     const paginationEnd = paginationStart + this.recipesPerPage;
+//     this.visibleRecipes = this.recipes.slice(paginationStart, paginationEnd);
+
+//     this.setInvisibleGridItemsIfNeeded(paginationEnd);
+//   }
+
+//   private setInvisibleGridItemsIfNeeded(paginationEnd: number) {
+//     const diff = paginationEnd - this.recipes.length;
+//     if (diff > 0) {
+//       for (let i = 0; i < diff; i++) {
+//         this.visibleRecipes.push({ class: 'invisible' } as Recipe);
+//       }
+//     }
+//   }
+
+//   public goToPage(pageIndex: number) {
+//     this.activePage = pageIndex;
+//     this.setPagination();
+//   }
+
+//   public viewRecipe(recipe: Recipe) {
+//     this.router.navigateByUrl(`recipe-details/${recipe.id}`);
+//   }
+// }
--- src/app/home/recipe-details/recipe-details.component.ts
+++ src/app/home/recipe-details/recipe-details.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { RecipeService } from '../../recipe.service';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { Recipe } from '../../recipe';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { Ingredient } from '../../ingredient';
 
 @Component({
   selector: 'app-recipe-details',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    HttpClientModule,
+    CommonModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
+  providers: [RecipeService],
   templateUrl: './recipe-details.component.html',
-  styleUrl: './recipe-details.component.scss'
+  styleUrl: './recipe-details.component.scss',
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
+  private routerParams = inject(ActivatedRoute);
+  private router = inject(Router);
+  private service = inject(RecipeService);
+  public recipe!: Recipe;
+  public displayedColumns: string[] = ['name', 'quantity'];
+  public dataSource: Ingredient[] = [];
 
+  ngOnInit(): void {
+    this.getRouteParams();
+  }
+
+  private getRouteParams() {
+    this.routerParams.params.subscribe((data) => {
+      this.getRecipeById(+data['id']);
+    });
+  }
+
+  private getRecipeById(id: number) {
+    this.service.getAllRecipes().subscribe((recipes) => {
+      const allRecipes = recipes.map((item, index) => {
+        return { ...item, id: index + 1 };
+      });
+      this.recipe = allRecipes.find((x) => x.id == id) || ({} as Recipe);
+      this.dataSource = this.recipe.Ingredients;
+    });
+  }
+
+  public goBack() {
+    this.router.navigateByUrl('/');
+  }
+
+  public goToSite() {
+    window.open(this.recipe.recipe_url, '_blank');
+  }
 }
