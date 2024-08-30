6ad2d151c65ed394ab9238413f12301ef9cc1c48
Lilahjane <lilahjane25@gmail.com>
2024-08-30 11:59:56 -0400

    test5

--- src/app/school/noedit/noedit.component.ts
+++ src/app/school/noedit/noedit.component.ts
@@ -1,43 +1,44 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { ITSenior } from '../itsenior';
 import { HttpClientModule } from '@angular/common/http';
 import { ITSeniorService } from '../itsenior.service';
 
-
-
-
 @Component({
   selector: 'app-noedit',
   templateUrl: './noedit.component.html',
-  styleUrl: './noedit.component.scss',
+  styleUrls: ['./noedit.component.scss'],  // Fixed the typo from 'styleUrl' to 'styleUrls'
   standalone: true,
   imports: [
     AsyncPipe,
     MatTableModule,
+    MatPaginatorModule,  // Import the MatPaginatorModule
     CommonModule,
     HttpClientModule
   ],
   providers: [ITSeniorService]
 })
-
-export class NoeditComponent {
+export class NoeditComponent implements OnInit, AfterViewInit {
   private service = inject(ITSeniorService);
 
-  isplayedColumns: string[] = ['Title', 'Due_Date', 'Due_Time', 'Max_Points', 'Course', 'Assignment_Link',];
+  displayedColumns: string[] = ['Title', 'Due_Date', 'Due_Time', 'Max_Points', 'Course', 'Assignment_Link'];
+  dataSource = new MatTableDataSource<ITSenior>();
 
-  dataSource = New ;MatTableDataSource<IT_Seniors>();
+  // ViewChild to access the paginator component
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
-  ngOnInit(): void{
+  ngOnInit(): void {
+    // Fetch data from service and assign it to the data source
     this.service.getAllAssignments().subscribe((IT_Seniors) => {
       this.dataSource.data = IT_Seniors;
     });
   }
 
-//   displayedColumns: string[] = ['Title', 'Due_Date', 'Due_Time', 'Max_Points', 'Course', 'Assignment_link',];
-//   dataSource = ELEMENT_DATA;
-// ELEMENT_DATA: any;
+  ngAfterViewInit(): void {
+    // Assign the paginator to the MatTableDataSource after view initialization
+    this.dataSource.paginator = this.paginator;
+  }
 }
-
-
